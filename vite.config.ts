4ebf64c578dba27b506f516909737837546c0742
Elias <elias.bobadilla@gmail.com>
2022-08-15 11:53:44 -0500

    fix gp error

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { env } from 'node:process'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
+      jsxImportSource: '@emotion/react',
       babel: {
-        plugins: ["@emotion/babel-plugin"],
+        plugins: ['@emotion/babel-plugin'],
       },
     }),
   ],
-});
+  base: env.IS_GITHUB ? 'gillian' : undefined,
+})
